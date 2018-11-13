b126df558cb54e3bfc767a084611df88c7c43cf8
Payne <paynezh@yahoo.ca>
2018-11-13 07:35:34 -0500

    5.6 Using the AltContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,25 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes.js';
+import AltContainer from 'alt-container';
 
+import Notes from './Notes.js';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 
-  //state = {
-  //  notes: [
-  //    {
-  //      id: uuid.v4(),
-  //      task: 'Learn Webpack'
-  //    },
-  //    {
-  //      id: uuid.v4(),
-  //      task: 'Learn React'
-  //    },
-  //    {
-  //      id: uuid.v4(),
-  //      task: 'Do laundry'
-  //    }
-  //  ]
-  //};
-
-  state = NoteStore.getState();
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    // Without a property initializer `this` wouldn't
-    // point at the right context because it defaults to
-    // `undefined` in strict mode.
-    this.setState(state);
-  };
-
   render() {
-    const notes = this.state.notes;
-
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
