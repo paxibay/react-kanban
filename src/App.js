05b84c4e03a1ad78bd620106d214de88b6cf67a4
Payne <paynezh@yahoo.ca>
2018-11-12 21:21:40 -0500

    4.2 Connecting Data with App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import uuid from 'uuid';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          {notes.map(note =>
+            <li key={note.id}>{note.task}</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
-
-export default App;
