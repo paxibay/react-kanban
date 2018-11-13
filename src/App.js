b24321595802c060b6cdb3d2fd756cc1ba7c80f7
Payne <paynezh@yahoo.ca>
2018-11-12 21:38:04 -0500

    4.3 Adding New Items to the List

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import uuid from 'uuid';
 
 export default class App extends React.Component {
-  render() {
-    const notes = [
+
+  state = {
+    notes: [
       {
         id: uuid.v4(),
         task: 'Learn Webpack'
@@ -16,16 +17,42 @@ export default class App extends React.Component {
         id: uuid.v4(),
         task: 'Do laundry'
       }
-    ];
+    ]
+  };
+
+
+  render() {
+    const notes = this.state.notes;
 
     return (
       <div>
-        <ul>
-          {notes.map(note =>
-            <li key={note.id}>{note.task}</li>
-          )}
-        </ul>
+        <button onClick={this.addNote}>+</button>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
       </div>
     );
   }
+
+  // this.setState accepts a second parameter like this: 
+  // this.setState({ ...}, () => console.log('set state!')).
+  // This is handy to know if you want to trigger some 
+  // behavior right after setState has completed
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+
+    // or
+
+    this.setState({
+      notes: [...this.state.notes, {
+        id: uuid.v4(),
+        task: 'New task'
+      }]
+    })
+  };
 }
