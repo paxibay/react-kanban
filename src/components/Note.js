0137140a100b943a78f61943935e3eb9c4c90cde
Payne <paynezh@yahoo.ca>
2018-11-12 22:58:28 -0500

    4.7 Styling Application

--- src/components/Note.js
+++ src/components/Note.js
@@ -43,14 +43,16 @@ export default class Note extends React.Component {
     const onDelete = this.props.onDelete;
     return (
       <div onClick={this.edit}>
-        <span>{this.props.task}</span>
+        <span className="task">{this.props.task}</span>
         {onDelete ? this.renderDelete() : null}
       </div>
     );
   };
 
   renderDelete = () => {
-    return <button onClick={this.props.onDelete}>x</button>;
+    return <button
+      className="delete-note"
+      onClick={this.props.onDelete}>x</button>;
   };
 
   edit = () => {
